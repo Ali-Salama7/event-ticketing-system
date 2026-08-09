cfb802939ada0c75f6434470d4c1bd8b8d827277
Ali Salama <alisalamadev@gmail.com>
2026-08-09 22:31:02 +0300

    feat: add redis connection setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config'
+import redis  from './config/redis.js'
 import authRoute from './auth/authRoutes.js'
 
 const app = express()
@@ -7,7 +8,6 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 app.use('/auth', authRoute)
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
