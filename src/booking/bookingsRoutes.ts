b6bdea76254c5369889511d59b31132b90ad4194
Ali Salama <alisalamadev@gmail.com>
2026-08-13 05:26:38 +0300

    feat: add booking confirmation with transaction and seat unlock

--- src/booking/bookingsRoutes.ts
+++ src/booking/bookingsRoutes.ts
@@ -6,5 +6,6 @@ const router = Router()
 const bookingsController = new BookingsController()
 
 router.post('/lock', authMiddleware,(req, res, next) => bookingsController.lockSeat(req, res, next))
+router.post('/confirm', authMiddleware,(req, res, next) => bookingsController.confirmBooking(req, res, next))
 
 export default router
--- src/booking/bookingsService.ts
+++ src/booking/bookingsService.ts
@@ -1,6 +1,7 @@
 import prisma from "../config/db.js";
-import { lockSeat as redisLockSeat } from "./redisLock.js";
-import { BadRequestError, NotFoundError } from "../shared/errors.js";
+import { lockSeat as redisLockSeat, unlockSeat } from "./redisLock.js";
+import { BadRequestError, ForbiddenError, NotFoundError } from "../shared/errors.js";
+import redis from "../config/redis.js";
 
 export class BookingsService{
     async lockSeat(userId: number,lockData: {seatId: number}){
@@ -19,4 +20,36 @@ export class BookingsService{
         await redisLockSeat(userId, lockData.seatId)
         return { message: "Seat locked successfully", seatId: lockData.seatId }
     }
+
+    async confirmBooking(userId: number, seatId: number){
+        const result = await redis.get(`seat:${seatId}:lock`)
+
+        if(result == null){
+            throw new BadRequestError("No active lock found for this seat. Please lock the seat first.");
+        }
+
+        if(Number(result) !== userId){
+            throw new ForbiddenError("This seat is locked by another user");
+        }
+
+        const booking =  await prisma.$transaction(async (tx) => {
+            const createBooking = await tx.booking.create({
+                data: {
+                    userId: userId,
+                    seatId: seatId
+                }
+            })
+
+            const updateStatus = await tx.seat.update({
+                where: {id: seatId},
+                data: {
+                    seatsStatus: 'BOOKED'
+                }
+            })
+            return createBooking
+        })
+        await unlockSeat(seatId, userId)
+        return booking
+    }
+
 }
